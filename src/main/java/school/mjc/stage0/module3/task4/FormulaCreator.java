b3daf9e707a999005cf6c32e97501a8db6d67d9e
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-01 18:02:24 +0600

    FormulaCreator and IntRepresentation tasks commit after the first testing

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a=5, b=3;
 
-        System.out.println((9*a*a - 5*b + 2 + a - 7) * ((a + b + 4*a*b) / 2));
+        System.out.println((9*a*a - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2));
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c', h = 'h', a = 'a', r = 'r';
-        System.out.println("c = " + c + ", h = " + h + ", a = " + a + ", r = " + r);
+        int c = 99, h = 104, a = 97, r = 114;
+        System.out.println((char) c);
+        System.out.println((char) h);
+        System.out.println((char) a);
+        System.out.println((char) r);
     }
 }
